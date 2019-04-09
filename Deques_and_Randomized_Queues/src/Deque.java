85414f232117ea6fb98bdf284637ce5c47acec53
Dongqin Zhou <dongqinzhou1110@gmail.com>
2019-04-09 21:34:49 +0800

    not done

--- Deques_and_Randomized_Queues/src/Deque.java
+++ Deques_and_Randomized_Queues/src/Deque.java
@@ -1,12 +1,19 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
 public class Deque<Item> implements Iterable<Item> {
     private int size = 0;
+    private Node first;
+    
     private class Node{
         Item item;
         Node next;
         Node previous;
     }
     public Deque() {
-
+        first = null;
+        size = 0;
     }                          // construct an empty deque
     public boolean isEmpty(){
         return size()==0;
@@ -20,8 +27,32 @@ public class Deque<Item> implements Iterable<Item> {
     public void addLast(Item item)   {
         throw new IllegalArgumentException("cannot add null");
     }        // add the item to the end
-    public Item removeFirst()                // remove and return the item from the front
-    public Item removeLast()               // remove and return the item from the end
-    public Iterator<Item> iterator() ;        // return an iterator over items in order from front to end
-    public static void main(String[] args)   // unit testing (optional)
+    public Item removeFirst(){
+        if (isEmpty())
+            throw new NoSuchElementException("deque is empty, cannot remove");
+        Item item = first.item;
+        return item;
+    }                // remove and return the item from the front
+    public Item removeLast(){
+        if (isEmpty())
+            throw new NoSuchElementException("deque is empty, cannot remove");
+        Item item = first.item;
+        return item;
+    }               // remove and return the item from the end
+    public Iterator<Item> iterator(){return new DequeIterator();}       // return an iterator over items in order from front to end
+    private class DequeIterator implements Iterator<Item>{
+        private Node current = first;
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException("no next item");
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+    public static void main(String[] args) {
+
+    }  // unit testing (optional)
 }
