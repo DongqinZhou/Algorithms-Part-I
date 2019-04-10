74eac9499e69e691d5e21d605f0687f7d0982d2f
Dongqin Zhou <dongqinzhou1110@gmail.com>
2019-04-10 14:13:19 +0800

    Change from (first:null + last:null) to (current:null)

--- Deques_and_Randomized_Queues/src/Deque.java
+++ Deques_and_Randomized_Queues/src/Deque.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -5,45 +7,79 @@ import java.util.NoSuchElementException;
 public class Deque<Item> implements Iterable<Item> {
     private int size = 0;
     private Node first;
-    
+    private Node last;
+
     private class Node{
         Item item;
         Node next;
         Node previous;
     }
-    public Deque() {
+    public Deque() { // construct an empty deque
         first = null;
+        last = null;
         size = 0;
-    }                          // construct an empty deque
+    }
     public boolean isEmpty(){
         return size()==0;
     }                 // is the deque empty?
     public int size(){
         return size;
     }                        // return the number of items on the deque
-    public void addFirst(Item item){
-        throw new IllegalArgumentException("cannot add null");
-    }      // add the item to the front
-    public void addLast(Item item)   {
-        throw new IllegalArgumentException("cannot add null");
-    }        // add the item to the end
-    public Item removeFirst(){
+    public void addFirst(Item item){                            // add the item to the front
+        if (item == null)
+            throw new IllegalArgumentException("cannot add null");
+        if (size == 0){
+            first = new Node();
+            first.item = item;
+            first.next = last;
+            size++;
+        }
+        else {
+            Node oldfirst = first;
+            first = new Node();
+            first.item = item;
+            first.next = oldfirst;
+            size++;
+        }
+    }
+    public void addLast(Item item)   {                  // add the item to the end
+        if (item == null)
+            throw new IllegalArgumentException("cannot add null");
+        if (size ==0){
+            last = new Node();
+            last.item = item;
+            last.previous = first;
+            size++;
+        }
+        else{
+            Node oldlast = last;
+            last = new Node();
+            last.item = item;
+            last.previous = oldlast;
+            size++;
+        }
+    }
+    public Item removeFirst(){                      // remove and return the item from the front
         if (isEmpty())
             throw new NoSuchElementException("deque is empty, cannot remove");
         Item item = first.item;
+        first = first.next;
+        size--;
         return item;
-    }                // remove and return the item from the front
-    public Item removeLast(){
+    }
+    public Item removeLast(){               // remove and return the item from the end
         if (isEmpty())
             throw new NoSuchElementException("deque is empty, cannot remove");
-        Item item = first.item;
+        Item item = last.item;
+        last = last.previous;
+        size--;
         return item;
-    }               // remove and return the item from the end
+    }
     public Iterator<Item> iterator(){return new DequeIterator();}       // return an iterator over items in order from front to end
     private class DequeIterator implements Iterator<Item>{
         private Node current = first;
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
+        public boolean hasNext()  { return current != null; }
+        public void remove()      { throw new UnsupportedOperationException("baby don't do this to me");  }
 
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException("no next item");
@@ -52,7 +88,11 @@ public class Deque<Item> implements Iterable<Item> {
             return item;
         }
     }
-    public static void main(String[] args) {
-
-    }  // unit testing (optional)
+    public static void main(String[] args) { // unit testing (optional)
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(15);
+        deque.addLast(18);
+        StdOut.println(deque.removeFirst());
+        StdOut.println(deque.removeFirst());
+    }
 }
