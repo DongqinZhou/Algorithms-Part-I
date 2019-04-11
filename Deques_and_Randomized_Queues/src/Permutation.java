ab9c5384f69ada1261a27da580b9ad028c5b6d95
Dongqin Zhou <dongqinzhou1110@gmail.com>
2019-04-11 20:34:04 +0800

    Success!

--- Deques_and_Randomized_Queues/src/Permutation.java
+++ Deques_and_Randomized_Queues/src/Permutation.java
@@ -1,25 +1,18 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args){
         RandomizedQueue<String> RQ = new RandomizedQueue<>();
-        StdOut.println("Please input the number of strings for input:");
-        int n = StdIn.readInt();
-        StdOut.println("Please input the strings:");
-        while (!StdIn.isEmpty()){
-            String input = StdIn.readString();
-            RQ.enqueue(input);
-        }
-        StdOut.println("Please input the number of strings for output (k <= n):");
-        int k = StdIn.readInt();
-        while (k > n){
-            StdOut.println("Please input the number of strings for output (k <= n):");
-            k = StdIn.readInt();
-        }
-        for (int j = 0; j < k; j++) {
-            StdOut.println(RQ.sample());
+        int k = Integer.parseInt(args[0]);
+        while (!StdIn.isEmpty())
+            RQ.enqueue(StdIn.readString());
+        int i = 0;
+        for (String rq : RQ) {
+            StdOut.println(rq);
+            i++;
+            if (i == k)
+                break;
         }
     }
 }
